2db4a6f4d99cffc93141d8510344e78d683a0980
Z4 DEV <z4kreyyt@gmail.com>
2023-08-27 02:28:14 +0100

    SideBar Modification -> Active Verification

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,11 +1,43 @@
 import * as C from './style';
+import { useState } from 'react';
+import { FaHouseUser } from 'react-icons/fa';
+import { MdFunctions } from 'react-icons/md';
+import { MdOutlineScience } from 'react-icons/md';
 
 export const SideBar = () => {
+    const [active, setActive] = useState(0);
+
     return (
         <C.Container>
             <C.SideBarTitle>CoralMC</C.SideBarTitle>
             <C.SideBarList>
-                <C.SideBarListItem>SASASA</C.SideBarListItem>
+                <C.SideBarListItem 
+                    active={active == 0 ? true : false}
+                    onClick={() => {
+                        setActive(0);
+                    }}
+                >
+                    <FaHouseUser color={"9CA3AF"} size={17}/>
+                    Control Panel
+                </C.SideBarListItem>
+                <C.SideBarListItem 
+                    active={active == 1 ? true : false}
+                    onClick={() => {
+                        setActive(1);
+                    }}
+                >
+                    <MdFunctions color={"9CA3AF"} size={17}/>
+                    Variables
+                </C.SideBarListItem>
+                <C.SideBarListItem 
+                    active={active == 2 ? true : false}
+                    onClick={() => {
+                        setActive(2);
+                    }}
+                >
+                    <MdOutlineScience color={"9CA3AF"} size={17}/>
+                    Settings
+                </C.SideBarListItem>
             </C.SideBarList>
         </C.Container>
     )
--- src/components/SideBar/style.ts
+++ src/components/SideBar/style.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+interface SideBarListItemProps {
+    active?: boolean;
+}
+
 export const Container = styled.div`
     width: 300px;
     height: 100%;
@@ -13,8 +17,7 @@ export const Container = styled.div`
 export const SideBarTitle = styled.h1`
     font-family: 'Inter', sans-serif;
     font-size: 17px;
-    color: #fff;
-    opacity: 0.8;
+    color: #9CA3AF;
 `
 
 export const SideBarList = styled.ul`
@@ -22,12 +25,36 @@ export const SideBarList = styled.ul`
     list-style: none;
     margin-top: 20px;
     display: flex;
-    gap: 10px;
+    gap: 7px;
     flex-direction: column;
     justify-content: flex-start;
     padding: 20px;
 `
 
-export const SideBarListItem = styled.li`
+export const SideBarListItem = styled.li<SideBarListItemProps>`
+    padding: 7px;
+    border-radius: 8px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    font-family: 'Inter', sans-serif;
+    font-size: 15px;
+    color: #9CA3AF;
 
+    transition: all 0.2s linear;
+
+    &:hover {
+        cursor: pointer;
+        background-color: #374151;
+        opacity: 0.8;
+    }
+
+    ${props => props.active && `
+        background-color: #374151;
+
+        &:hover {
+            opacity: 1;
+        }
+    `}
 `
